2908212447130fa948c7624fb5229575b34c11c1
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 11:38:16 +0300

    Update route to serve index.html file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ let app = express();
 
 console.log('Hello World');
 
+absolutePath = __dirname + '/views/index.html';
+
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(absolutePath);
 });
 
 module.exports = app;
