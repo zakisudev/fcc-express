b25847b9586f4030fa1f4312e87fd799a7ab5843
Zekaria Mohammed <zakisu250@gmail.com>
2024-01-09 11:56:15 +0300

    Refactor JSON response to support uppercase message style

--- myApp.js
+++ myApp.js
@@ -12,7 +12,11 @@ app.get('/', (req, res) => {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', (req, res) => {
-  res.json({ message: 'Hello json' });
+  let message = 'Hello json';
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
 });
 
 module.exports = app;
